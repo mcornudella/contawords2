c302bb9c6faf02c209a37aa65db6151dca44d563
mcornudella <37551814+mcornudella@users.noreply.github.com>
2018-08-01 16:16:53 +0200

    Update pipa_contawords_URL_list.sh

--- scripts/pipa_contawords_URL_list.sh
+++ scripts/pipa_contawords_URL_list.sh
@@ -5,8 +5,8 @@
 # -----------------------------------------------------------------------------
 set -e
 
-scripts_dir="/Users/miquelcornudella/Documents/IULA/tasques/contawords/scripts"
-output_dir="/Users/miquelcornudella/Documents/IULA/tasques/contawords/public/system/output_files"
+scripts_dir="/mnt/vmdata/devel-trl/var/rails/contawords2/scripts"
+output_dir="/mnt/vmdata/devel-trl/var/rails/contawords2/public/system/output_files"
 
 inputFile=$1
 lang=$2
@@ -16,21 +16,18 @@ lang=$2
 #echo "Input params"
 #echo $inputFile
 #echo $lang
+
 #echo ""
 #echo "Step 1. Call callFreelingGivenURL_text_list.py"
 outputFileFL=$(/usr/bin/python $scripts_dir/callFreelingGivenURL_text_list.py "$inputFile" $lang 2>&1 )
-
-
 outputFileFL="$(echo $outputFileFL | cut -d' ' -f1)"
-
 #echo "outputFileFL: " $outputFileFL
 
 #echo ""
 #echo "Step 2. Call cqp_index.sh"
-#$scripts_dir/cqp_index.sh -f $outputFileFL -s "$scripts_dir/cqp_struct.txt"
 corpusID=$( $scripts_dir/cqp_index.sh -f $outputFileFL -s "$scripts_dir/cqp_struct.txt" 2>&1 ) 
 corpusID="$(echo $corpusID | cut -d' ' -f1)"
-#echo $corpusID
+#echo "CorpusID: "$corpusID
 
 #echo ""
 #echo "Step 3. Call corpus_analysis.sh"
