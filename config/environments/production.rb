b3ab043e2b91b0e72438b7895a83e7413e8f4179
mcornudella <37551814+mcornudella@users.noreply.github.com>
2018-10-17 12:40:41 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -92,3 +93,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+UPLOADED_FILES_BASE_URL = "contawords.iula.upf.edu:3110"
